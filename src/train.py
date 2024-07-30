f846770a122e57dce9d6687507bc318e1c2daca3
Ra1nek <146630228+Ra1nek@users.noreply.github.com>
2024-07-30 15:23:35 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,10 +1,12 @@
-# scripts/train.py
-import os
+import sys
 from TTS.bin.train import main as tts_train
 
 def train_model(config_path):
     tts_train(config_path=config_path)
 
 if __name__ == "__main__":
-    config_path = "config/config.json"
+    if len(sys.argv) != 2:
+        print("Usage: python train.py <config_path>")
+        sys.exit(1)
+    config_path = sys.argv[1]
     train_model(config_path)
